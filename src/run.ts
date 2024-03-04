50f644947cc857212d637f9900d31022c4973bd0
wandyezj <wandyezj@gmail.com>
2024-03-03 17:14:24 -0800

    load run libraries

--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,77 @@
 import { loadSnip } from "./core/storage";
 console.log("run");
 
-function initialize() {
+type LibType = "css" | "js" | undefined;
+
+async function loadScript(lib: string) {
+    return new Promise<void>((resolve, reject) => {
+        console.log("load - start: ", lib);
+        const script = document.createElement("script");
+        script.setAttribute("src", lib);
+        script.onload = () => {
+            console.log("load - complete: ", lib);
+            resolve();
+        };
+        document.head.appendChild(script);
+    });
+}
+
+async function loadLibraries(libraries: string) {
+    // trim and remove empty lines
+    const libs = libraries
+        .split("\n")
+        .map((lib) => lib.trim())
+        .filter((lib) => lib !== "")
+        .map((lib) => {
+            let libType: LibType = undefined;
+            const isLink = lib.startsWith("http://") || lib.startsWith("https://");
+            if (isLink) {
+                if (lib.endsWith(".css")) {
+                    libType = "css";
+                } else if (lib.endsWith(".js")) {
+                    libType = "js";
+                }
+            }
+
+            return {
+                lib,
+                libType,
+            };
+        });
+
+    const js = libs.filter(({ libType }) => libType === "js").map(({ lib }) => lib);
+    const css = libs.filter(({ libType }) => libType === "css").map(({ lib }) => lib);
+
+    // load js first
+    await Promise.all(js.map(loadScript));
+
+    // load css
+    css.forEach((lib) => {
+        console.log("load: ", lib);
+        const style = document.createElement("link");
+        style.rel = "stylesheet";
+        style.href = lib;
+        document.head.appendChild(style);
+    });
+}
+
+function loadCss(css: string) {
+    const style = document.createElement("style");
+    style.innerHTML = css;
+    document.head.appendChild(style);
+}
+
+function loadHtml(html: string) {
+    document.body.innerHTML = html;
+}
+
+function loadJs(js: string) {
+    const script = document.createElement("script");
+    script.innerHTML = js;
+    document.head.appendChild(script);
+}
+
+async function initialize() {
     const snip = loadSnip();
     console.log("snip", snip);
     if (snip === undefined) {
@@ -9,23 +79,17 @@ function initialize() {
     }
 
     // Content
+    const libraries = snip.files["libraries"].content;
     const css = snip.files["css"].content;
     const html = snip.files["html"].content;
     // TODO: will need to compile TypeScript, where should this be done?
     const js = snip.files["typescript"].content;
 
-    // insert css
-    const style = document.createElement("style");
-    style.innerHTML = css;
-    document.head.appendChild(style);
-
-    // insert html
-    document.body.innerHTML = html;
-
-    // insert js
-    const script = document.createElement("script");
-    script.innerHTML = js;
-    document.head.appendChild(script);
+    // Loading order matters
+    await loadLibraries(libraries);
+    loadCss(css);
+    loadHtml(html);
+    loadJs(js);
 }
 
 initialize();
