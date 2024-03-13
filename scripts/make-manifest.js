076554e414b03ee38c9864222666c93828c345af
jowandy <wandyezj@gmail.com>
2024-03-13 15:50:32 -0700

    manifests

--- scripts/make-manifest.js
+++ scripts/make-manifest.js
@@ -18,7 +18,27 @@ function clean(data) {
 }
 
 /**
- * make manifest for production
+ * make manifest for localhost from template
+ * @param {string} data
+ */
+function localhost(data) {
+    const duplicate = data.split("<!-- Word - Start -->")[1].split("<!-- Word - End -->")[0];
+
+    // Place place template duplicates on Excel and PowerPoint
+    data = data.replaceAll(
+        "<!-- Duplicate:(Word) Replace:(Document,Workbook) -->",
+        duplicate.replaceAll("Document", "Workbook")
+    );
+    data = data.replaceAll(
+        "<!-- Duplicate:(Word) Replace:(Document,Presentation) -->",
+        duplicate.replaceAll("Document", "Presentation")
+    );
+
+    return clean(data);
+}
+
+/**
+ * make manifest for production from localhost
  * @param {string} manifest
  */
 function production(data) {
@@ -42,11 +62,17 @@ function production(data) {
 function main() {
     const fs = require("fs");
 
+    const templateManifestPath = "./manifests/manifest-template.xml";
     const localManifestPath = "./manifests/manifest-local.xml";
     const prodManifestPath = "./manifests/manifest.xml";
 
-    const data = fs.readFileSync(localManifestPath, { encoding: "utf-8" });
-    fs.writeFileSync(prodManifestPath, production(data));
+    const data = fs.readFileSync(templateManifestPath, { encoding: "utf-8" });
+
+    const localhostData = localhost(data);
+    fs.writeFileSync(localManifestPath, localhostData);
+
+    const productionData = production(localhostData);
+    fs.writeFileSync(prodManifestPath, productionData);
 }
 
 main();
