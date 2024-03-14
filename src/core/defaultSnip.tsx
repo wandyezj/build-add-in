2fc2267bcbd994c2163d41a244e966efe840db01
jowandy <wandyezj@gmail.com>
2024-03-14 15:28:19 -0700

    update sample

--- src/core/defaultSnip.tsx
+++ src/core/defaultSnip.tsx
@@ -5,28 +5,49 @@ export const defaultSnip: Omit<Snip, "id"> = {
     files: {
         typescript: {
             language: "typescript",
-            content: `function x() {
+            content: `
+//
+// Click "Run" to execute the code below.
+//
+
+function ready() {
     console.log('Hello world!');
 }
 
 Office.onReady(({host, platform})=> {
     console.log("READY");
-    document.getElementById('host').innerText = \`Host: \${host}\`;
-    document.getElementById('platform').innerText = \`Platform: \${platform}\`;
-})`,
+
+    const elementHost = document.getElementById('host');
+    elementHost.innerText = host.toString();
+    elementHost.style.color = getHostColor(host);
+
+    document.getElementById('platform').innerText = platform.toString();
+});
+
+/**
+ * Get a color associated with the host.
+ */
+function getHostColor(host: Office.HostType): string {
+    const hostToColor = new Map<Office.HostType, string>([
+        [Office.HostType.Excel, "green"],
+        [Office.HostType.Word, "blue"],
+        [Office.HostType.PowerPoint, "orange"],
+    ]);
+    const color = hostToColor.get(host) || "purple";
+    return color;
+}
+`,
         },
         html: {
             language: "html",
             content: `
-<h1>Hello world!</h1>
-<h2 id="host"></h2>
-<h2 id="platform"></h2>
-        `,
+<h1 onclick="ready">Hello <span id="host">?</span> on <span id="platform">?</span>!</h1>
+`,
         },
         css: {
             language: "css",
             content: `h1 { 
-                color: red;
+    color: black;
 }`,
         },
         libraries: {
