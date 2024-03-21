46bf73f4f3bb209b0ad7837834d2db8e3d14f26a
jowandy <wandyezj@gmail.com>
2024-03-21 14:33:27 -0700

    update

--- src/core/compileCode.ts
+++ src/core/compileCode.ts
@@ -28,19 +28,26 @@ function createProgramFromFiles(
         ) => {
             const data = fileMap.get(fileName);
             if (data === undefined) {
+                console.log(`getSourceFile ${fileName} not found`);
                 return undefined;
             }
 
             return ts.createSourceFile(fileName, data, languageVersion, true);
         },
-        getDefaultLibFileName: (defaultLibOptions: ts.CompilerOptions) =>
-            "/" + ts.getDefaultLibFileName(defaultLibOptions),
-        writeFile: () => {}, // do nothing
+        // Is this deprecated?
+        getDefaultLibFileName: (defaultLibOptions: ts.CompilerOptions) => ts.getDefaultLibFileName(defaultLibOptions),
+        writeFile: (fileName, text) => {
+            console.log("writeFile");
+            console.log(fileName);
+            console.log(text);
+            return undefined; // don't need to write files
+        }, // do nothing
         getCurrentDirectory: () => "/",
         getDirectories: (/*path: string*/) => [],
         fileExists: (fileName: string) => {
-            console.log(`fileExists ${fileName}`);
-            return true; // all files exist
+            const exists = fileMap.has(fileName);
+            console.log(`fileExists ${fileName} ${exists}`);
+            return exists;
         },
         readFile: (fileName: string) => {
             console.log(`readfile ${fileName}`);
@@ -66,6 +73,10 @@ function createProgramFromFiles(
 
 function createProgram(code: string) {
     const target = ts.ScriptTarget.ES2022;
+    // Actually supply these libs from typescript as a string
+    // https://www.npmjs.com/package/@typescript/vfs
+    // add files as a JSON blob from typescript
+    // https://github.com/microsoft/TypeScript-Website/tree/v2/packages/typescript-vfs
     const lib = ["dom", "es2022"];
 
     const options: ts.CompilerOptions = {
@@ -73,7 +84,7 @@ function createProgram(code: string) {
         lib,
         module: ts.ModuleKind.None,
         strict: true,
-        noEmit: true,
+        //noEmit: true,
         inlineSourceMap: true,
         inlineSources: true,
     };
@@ -81,6 +92,56 @@ function createProgram(code: string) {
     const files = [{ name: "code.ts", data: code }];
     const program = createProgramFromFiles(files, { options });
     return program;
+}
+
+/**
+ * Compile TypeScript code to JavaScript
+ * @param code
+ * @returns text code
+ */
+export function compileCode(code: string) {
+    console.log("compileCode");
+    console.log(code);
+
+    const program = createProgram(code);
+
+    let issues: { message: string }[] = [];
+    const preDiagnostics = ts.getPreEmitDiagnostics(program);
+    if (preDiagnostics) {
+        issues = preDiagnostics.map((diagnostic) => {
+            const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
+            return {
+                message,
+            };
+        });
+    }
+    console.log(issues);
+
+    const file = program.getSourceFile("code.ts");
+    // the callback is synchronous ant use inside the code
+    let js: string = "";
+    const result = program.emit(file, (name, text) => {
+        console.log("emit writeFile");
+        console.log(name);
+        console.log(text);
+        js = text;
+    });
+    console.log(result);
+
+    const { diagnostics } = result;
+
+    if (diagnostics) {
+        issues = diagnostics.map((diagnostic) => {
+            const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
+            return {
+                message,
+            };
+        });
+    }
+
+    //const js = result.emittedFiles?.[0] || "";
+    console.log(js);
+    return { issues, js };
 }
 
 // function simpleCompile(code: string): string {
@@ -126,45 +187,3 @@ function createProgram(code: string) {
 //     console.log(js);
 //     return { issues, js };
 // }
-
-/**
- * Compile TypeScript code to JavaScript
- * @param code
- * @returns text code
- */
-export function compileCode(code: string) {
-    console.log("compileCode");
-    console.log(code);
-
-    const program = createProgram(code);
-
-    let issues: { message: string }[] = [];
-    const preDiagnostics = ts.getPreEmitDiagnostics(program);
-    if (preDiagnostics) {
-        issues = preDiagnostics.map((diagnostic) => {
-            const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
-            return {
-                message,
-            };
-        });
-    }
-    console.log(issues);
-
-    const result = program.emit();
-    console.log(result);
-
-    const { diagnostics } = result;
-
-    if (diagnostics) {
-        issues = diagnostics.map((diagnostic) => {
-            const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
-            return {
-                message,
-            };
-        });
-    }
-
-    const js = result.emittedFiles?.[0] || "";
-    console.log(js);
-    return { issues, js };
-}
