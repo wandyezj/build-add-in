c49608584a2abe889eca4875b3d9380694bd59bc
wandyezj <wandyezj@gmail.com>
2024-04-08 20:40:23 -0700

    links

--- src/help/App.tsx
+++ src/help/App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { FluentProvider, webLightTheme, Button } from "@fluentui/react-components";
+import React, { MouseEventHandler } from "react";
+import { FluentProvider, webLightTheme, Link } from "@fluentui/react-components";
 
+const tutorialText = "Tutorial";
 const tutorialUrl = "https://github.com/wandyezj/build-add-in/blob/main/docs/tutorial.md";
-function onClickTutorial() {
-    window.open(tutorialUrl, "_blank");
-}
+
+const apiReferenceText = "API Reference";
+const apiReferenceUrl = "https://learn.microsoft.com/en-us/javascript/api/overview?view=common-js-preview";
 
 /**
  * The top level application component.
@@ -12,9 +13,32 @@ function onClickTutorial() {
 export function App() {
     return (
         <FluentProvider theme={webLightTheme}>
-            <Button size={"large"} appearance={"primary"} onClick={onClickTutorial}>
-                Tutorial
-            </Button>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    minHeight: "100vh",
+                }}
+            >
+                <ButtonLink text={tutorialText} url={tutorialUrl} />
+                <br></br>
+                <br></br>
+                <ButtonLink text={apiReferenceText} url={apiReferenceUrl} />
+            </div>
         </FluentProvider>
+    );
+}
+
+function ButtonLink({ text, url }: { text: string; url: string }) {
+    function onClickTutorial() {
+        window.open(url, "_blank");
+    }
+
+    return (
+        <Link onClick={onClickTutorial} as="button">
+            {text}
+        </Link>
     );
 }
