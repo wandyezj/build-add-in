1b1978f2564998ccce829b3f6b38c66249862757
wandyezj <wandyezj@gmail.com>
2024-04-09 21:14:45 -0700

    hook

--- scripts/deploy-git-hooks.js
+++ scripts/deploy-git-hooks.js
@@ -3,12 +3,13 @@ const fs = require("fs");
 // Git Hooks
 // https://git-scm.com/book/en/v2/Customizing-Git-Git-Hooks
 
-const hookName = "pre-commit";
+const hookName = "pre-push";
 const hookPath = `.git/hooks/${hookName}`;
 const hookScript = `#!/bin/sh
 
 # run style before commit
-npm run style
+npm run style-check
+exit $?
 `;
 
 fs.writeFileSync(hookPath, hookScript);
