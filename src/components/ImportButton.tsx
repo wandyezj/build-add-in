e978e0de794e070c95907d730ba0ee56d693c6b6
wandyezj <wandyezj@gmail.com>
2024-04-09 17:15:19 -0700

    import

--- src/components/ImportButton.tsx
+++ src/components/ImportButton.tsx
@@ -14,6 +14,7 @@ import { ArrowDownloadRegular } from "@fluentui/react-icons";
 
 import { makeStyles, tokens, useId, Label, Textarea } from "@fluentui/react-components";
 import { LogTag, log } from "../core/log";
+import { getSnipFromJson } from "../core/Snip";
 
 const useStyles = makeStyles({
     base: {
@@ -25,15 +26,76 @@ const useStyles = makeStyles({
     },
 });
 
+function isValidSnip(value: string) {
+    const newSnip = getSnipFromJson(value);
+    const valid = newSnip !== undefined;
+    return valid;
+}
+
+async function fetchUrlText(url: string): Promise<string> {
+    const request = await fetch(url);
+    const text = await request.text();
+    return text;
+}
+
+function isPossibleUrl(value: string) {
+    const text = value.trim();
+    // Does it look like a url?
+    const possible = !text.includes("\n") && (text.startsWith("http://") || text.startsWith("https://"));
+    return possible;
+}
+
+function isPossibleGistUrl(value: string) {
+    const possible = isPossibleUrl(value) && value.startsWith("https://gist.github.com/");
+    return possible;
+}
+
+/**
+ * Value can be:
+ * - text - JSON snip
+ * - url - to a JSON snip
+ * - gist - GitHub gist url with a single file containing the JSON snip
+ * @param value
+ * @returns the snip text or undefined
+ */
+async function getImportSnip(value: string): Promise<string | undefined> {
+    // Import - text
+    let content = value;
+
+    try {
+        const url = value.trim();
+        if (isPossibleGistUrl(value)) {
+            // Import - gist
+            // TODO:
+        } else if (isPossibleUrl(value)) {
+            // Import - url
+            const text = await fetchUrlText(url);
+            content = text;
+        }
+    } catch (e) {
+        console.error(e);
+    }
+
+    const valid = isValidSnip(content);
+    if (valid) {
+        return content;
+    }
+    return undefined;
+}
+
 export function ImportButton({ setImport }: { setImport: (value: string) => void }) {
     const textareaId = useId("import-textarea");
     const styles = useStyles();
 
-    function onClickImport(event: React.FormEvent) {
+    async function onClickImport(event: React.FormEvent) {
         event.preventDefault();
         log(LogTag.ButtonImport, "import");
         const value = (document.getElementById(textareaId) as HTMLTextAreaElement).value;
-        setImport(value);
+        const snipText = await getImportSnip(value);
+        if (snipText) {
+            setImport(snipText);
+        }
+        // otherwise invalid.
     }
 
     return (
