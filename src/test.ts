dbe4acf8678a8fe08267f2bd4893fbd791540292
wandyezj <wandyezj@gmail.com>
2024-04-18 21:32:09 -0700

    test compress length

--- src/test.ts
+++ src/test.ts
@@ -7,6 +7,15 @@ function getButton(id: string) {
 function getTextArea(id: string) {
     return document.getElementById(id) as HTMLTextAreaElement;
 }
+function getSpan(id: string) {
+    return document.getElementById(id) as HTMLSpanElement;
+}
+
+function setCount(element: HTMLTextAreaElement, id: string) {
+    const text = element.value;
+    const length = text.length;
+    getSpan(id).innerText = length.toString();
+}
 
 function setup() {
     const buttonCompress = getButton("button-compress");
@@ -14,19 +23,26 @@ function setup() {
 
     const textareaText = getTextArea("textarea-text");
     const textareaCompressed = getTextArea("textarea-compressed");
-
     const textareaDecompressed = getTextArea("textarea-decompressed");
+
+    function setCounts() {
+        setCount(textareaText, "span-count-text");
+        setCount(textareaCompressed, "span-count-compressed");
+        setCount(textareaDecompressed, "span-count-decompressed");
+    }
 
     buttonCompress.onclick = async () => {
         const text = textareaText.value;
         const compressed = await compress(text);
         textareaCompressed.value = compressed;
+        setCounts();
     };
 
     buttonDecompress.onclick = async () => {
         const compressed = textareaCompressed.value;
         const decompressed = await decompress(compressed);
         textareaDecompressed.value = decompressed;
+        setCounts();
     };
 }
 
