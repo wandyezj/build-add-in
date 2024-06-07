c7f00816dd0ea5e246b76aa669f10fbbdb44f7ba
wandyezj <wandyezj@gmail.com>
2024-06-07 09:56:08 -0700

    refactor

--- src/actions.ts
+++ src/actions.ts
@@ -1,51 +1,10 @@
-import {
-    Action,
-    ActionType,
-    TriggerAction,
-    TriggerExcelNamedRangeEdit,
-    TriggerType,
-} from "./core/actions/TriggerAction";
+import { ActionType, TriggerAction, TriggerType } from "./core/actions/TriggerAction";
+import { logTriggerId, registerTriggerActions } from "./core/actions/triggerActionHandlers";
 
-// cspell:ignore SHOWTASKPANE HIDETASKPANE addin
 console.log("actions load");
 
-Office.actions.associate("SHOWTASKPANE", function () {
-    console.log("shortcut - Show");
-    return Office.addin
-        .showAsTaskpane()
-        .then(function () {
-            return;
-        })
-        .catch(function (error) {
-            return error.code;
-        });
-});
-
-Office.actions.associate("HIDETASKPANE", async function () {
-    console.log("shortcut - Hide");
-    try {
-        await Office.addin.hide();
-    } catch (e) {
-        console.log(e);
-    }
-});
-
-Office.actions.associate("SETCOLOR", function () {
-    console.log("shortcut - Set Color");
-    Excel.run(async (context) => {
-        const range = context.workbook.getSelectedRange();
-        range.format.fill.load("color");
-
-        await context.sync();
-        const colors = ["#FFFFFF", "#C7CC7A", "#7560BA", "#9DD9D2", "#FFE1A8", "#E26D5C"];
-        const colorIndex = (colors.indexOf(range.format.fill.color) + 1) % colors.length;
-        range.format.fill.color = colors[colorIndex];
-        await context.sync();
-    });
-});
-
-const triggerActionLoadLog: TriggerAction = {
-    id: "LoadLog",
+const triggerActionLoadLogTest: TriggerAction = {
+    id: "LoadLogTest",
     trigger: {
         type: TriggerType.Load,
     },
@@ -67,124 +26,54 @@ const triggerActionExcelNamedRangeTest: TriggerAction = {
     },
 };
 
+const triggerActionExcelWorksheetNameRangeAddressTest: TriggerAction = {
+    id: "Sheet1A1Edit",
+    trigger: {
+        type: TriggerType.ExcelWorksheetNameRangeAddressEdit,
+        parameters: {
+            worksheetName: "Sheet1",
+            rangeAddress: "A1",
+        },
+    },
+    action: {
+        type: ActionType.Callback,
+        parameters: {
+            callback: async (triggerAction: Readonly<TriggerAction>) => {
+                // Log that this was called.
+                logTriggerId(triggerAction);
+
+                await Excel.run(async (context) => {
+                    const sheet = context.workbook.worksheets.getItemOrNullObject("Sheet1");
+                    const range = sheet.getRange("A2");
+                    range.load("values");
+                    await context.sync();
+                    if (range.isNullObject) {
+                        return;
+                    }
+
+                    const value = range.values[0][0];
+                    let newValue = 0;
+                    if (typeof value === "number") {
+                        newValue = value + 1;
+                    }
+
+                    range.values = [[newValue]];
+                    await context.sync();
+                    console.log("Incremented Value");
+                });
+            },
+        },
+    },
+};
+
 /**
  * action registry
  */
-const triggerActions: TriggerAction[] = [triggerActionLoadLog, triggerActionExcelNamedRangeTest];
-
-function runActionLogId(triggerAction: TriggerAction) {
-    const { id } = triggerAction;
-    console.log(`Trigger: [${id}]`);
-}
-
-/**
- * Actions types to runs.
- */
-const actionMap = new Map<string, (triggerAction: TriggerAction) => void>([[ActionType.LogId, runActionLogId]]);
-
-function runAction(triggerAction: TriggerAction) {
-    const actionType = triggerAction.action.type;
-    const handler = actionMap.get(actionType);
-
-    if (handler === undefined) {
-        console.log(`Handler for actionType ${actionType}`);
-    } else {
-        handler(triggerAction);
-    }
-}
-
-function getTriggerTypes(triggerActions: TriggerAction[], triggerType: TriggerType) {
-    const triggers = triggerActions.filter((triggerAction) => triggerAction.trigger.type === triggerType);
-    return triggers;
-}
-
-function hasTriggerType(triggerActions: TriggerAction[], triggerType: TriggerType) {
-    const triggers = getTriggerTypes(triggerActions, triggerType);
-    return triggers.length > 0;
-}
-
-function runTriggersActions(triggers: TriggerAction[]) {
-    triggers.forEach((trigger) => {
-        runAction(trigger);
-    });
-}
-
-/**
- * Trigger all load triggers
- */
-function triggerLoad() {
-    const triggers = getTriggerTypes(triggerActions, TriggerType.Load);
-    runTriggersActions(triggers);
-}
-
-/**
- * Trigger all Excel Named Range Edit triggers relevant to the event.
- * @param event
- */
-async function triggerExcelNamedRangeEdit(event: { worksheetId: string; rangeAddress: string }) {
-    const triggers = getTriggerTypes(triggerActions, TriggerType.ExcelNamedRangeEdit) as {
-        id: string;
-        trigger: TriggerExcelNamedRangeEdit;
-        action: Action;
-    }[];
-
-    // Check if the Range edit action matches a trigger
-    const matching = await Excel.run(async (context) => {
-        const workbook = context.workbook;
-
-        const intersects = triggers.map((trigger, index) => {
-            const namedItemName = trigger.trigger.parameters.namedRangeName;
-            const namedItem = workbook.names.getItemOrNullObject(namedItemName);
-            const range = namedItem.getRangeOrNullObject();
-
-            const eventRange = workbook.worksheets.getItemOrNullObject(event.worksheetId).getRange(event.rangeAddress);
-            const intersection = range.getIntersectionOrNullObject(eventRange);
-            return { intersection, index };
-        });
-
-        await context.sync();
-
-        const match = intersects
-            .filter(({ intersection }) => !intersection.isNullObject)
-            .map(({ index }) => triggers[index]);
-        return match;
-    });
-
-    runTriggersActions(matching);
-}
-
-async function handleWorksheetsChanged(event: Excel.WorksheetChangedEventArgs) {
-    // Filter for local range edits not made by the Add-In
-    if (
-        event.source === Excel.EventSource.local &&
-        event.triggerSource !== Excel.EventTriggerSource.thisLocalAddin &&
-        event.changeType === Excel.DataChangeType.rangeEdited
-    ) {
-        console.log("Trigger");
-
-        const eventWorksheetId = event.worksheetId;
-        const eventAddress = event.address;
-        triggerExcelNamedRangeEdit({ worksheetId: eventWorksheetId, rangeAddress: eventAddress });
-    }
-}
-
-//async function registerTriggerExcelNamedRangeEdit() {}
-
-async function registerTriggerActions() {
-    // Trigger on an edit to a named range.
-
-    const hasTriggerExcelNamedRangeEdit = hasTriggerType(triggerActions, TriggerType.ExcelNamedRangeEdit);
-
-    // Register an event that triggers when a worksheet is changed from a user local edit.
-    await Excel.run(async (context) => {
-        const workbook = context.workbook;
-        const worksheets = workbook.worksheets;
-
-        if (hasTriggerExcelNamedRangeEdit) {
-            worksheets.onChanged.add(handleWorksheetsChanged);
-        }
-    });
-}
+export const triggerActions: TriggerAction[] = [
+    triggerActionLoadLogTest,
+    triggerActionExcelNamedRangeTest,
+    triggerActionExcelWorksheetNameRangeAddressTest,
+];
 
 // target range
 // const namedRange = "test";
@@ -216,8 +105,6 @@ Office.onReady(() => {
     console.log("ready");
     // Requires trigger of the runtime before it will automatically start up.
     Office.addin.setStartupBehavior(Office.StartupBehavior.load);
-
-    triggerLoad();
 
     registerTriggerActions();
 });
--- src/core/actions/TriggerAction.ts
+++ src/core/actions/TriggerAction.ts
@@ -7,6 +7,11 @@ export enum TriggerType {
      * When an excel named range is edited
      */
     ExcelNamedRangeEdit = "ExcelNamedRangeEdit",
+
+    /**
+     * When an excel worksheet (by name) range (by address) is edited
+     */
+    ExcelWorksheetNameRangeAddressEdit = "ExcelWorksheetNameRangeAddressEdit",
 }
 
 export interface TriggerLoad {
@@ -23,20 +28,53 @@ export interface TriggerExcelNamedRangeEdit {
     };
 }
 
-export type Trigger = TriggerLoad | TriggerExcelNamedRangeEdit;
+export interface TriggerExcelWorksheetNameRangeAddressEdit {
+    type: TriggerType.ExcelWorksheetNameRangeAddressEdit;
+    parameters: {
+        /**
+         * name of the worksheet.
+         */
+        worksheetName: string;
+
+        /**
+         * address of the range.
+         */
+        rangeAddress: string;
+    };
+}
+
+export type Trigger = TriggerLoad | TriggerExcelNamedRangeEdit | TriggerExcelWorksheetNameRangeAddressEdit;
 
 export enum ActionType {
     /**
      * Log the id of the trigger;
      */
     LogId = "LogId",
+
+    /**
+     * Run a callback.
+     * note: this is an incredibly powerful action and shouldn't be serialized, but ok to use internally.
+     */
+    Callback = "Callback",
 }
 
 export interface ActionLogId {
     type: ActionType.LogId;
 }
 
-export type Action = ActionLogId;
+export interface ActionCallback {
+    type: ActionType.Callback;
+    parameters: {
+        /**
+         * Hardcoded callback to call.
+         * @param triggerAction the trigger action that triggered the callback.
+         * @returns
+         */
+        callback: (triggerAction: Readonly<TriggerAction>) => Promise<void>;
+    };
+}
+
+export type Action = ActionLogId | ActionCallback;
 
 export interface TriggerAction {
     /**
