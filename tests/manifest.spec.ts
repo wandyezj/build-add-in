cf7db067bb57d26168029e4b228a054330cef836
wandyezj <wandyezj@gmail.com>
2025-08-19 21:52:51 -0700

    Test for manifest files

--- tests/manifest.spec.ts
+++ tests/manifest.spec.ts
@@ -3,12 +3,18 @@ import { localDistManifestProductionPath, localDistManifestProductionOutlookPath
 import * as fs from "fs";
 import path from "path";
 
+/**
+ * Verify the Production manifests created by the build.
+ */
 test("Production manifests are free of (local)", async () => {
     const productionManifests = [localDistManifestProductionPath, localDistManifestProductionOutlookPath];
     productionManifests.forEach((manifestPath) => {
-        const data = fs.readFileSync(manifestPath);
-        // check for presence of (local) which should not happen.
+        // The build process should have created the manifest files - did you run `npm run build`?
+        expect(fs.existsSync(manifestPath), `manifestPath ${manifestPath} exists`).toBeTruthy();
 
+        const data = fs.readFileSync(manifestPath);
+
+        // check for presence of (local) which should not happen.
         const expectLocal = false;
         const hasLocal = data.includes("(local)");
         expect(hasLocal, `${path.basename(manifestPath)} replaces local`).toBe(expectLocal);
