fc3d127a3ec4a5ece99726b11531b3e825bef6a6
wandyezj <wandyezj@gmail.com>
2025-12-08 15:53:01 -0800

    Manifest

---
+++ scripts/create-unified-zip.js
@@ -0,0 +1,64 @@
+// Create a unified zip file
+// https://learn.microsoft.com/en-us/office/dev/add-ins/testing/sideload-add-in-with-unified-manifest#manually-create-the-add-in-package-file
+
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const child_process = require("child_process");
+
+const parameters = process.argv.slice(2);
+
+if (parameters.length !== 2) {
+    console.log("usage: [manifest.json] [output directory]");
+    process.exit(0);
+}
+
+const [manifestPath, outputDirectory] = parameters;
+
+const root = path.resolve(__dirname, "..");
+console.log(root);
+
+// Read manifest
+const manifestText = fs.readFileSync(manifestPath, "utf8");
+
+/**
+ * @type {{icons: { outline: string; color: string;}}}
+ */
+const manifest = JSON.parse(manifestText);
+
+// Read "icons" property
+const icons = [manifest.icons.color, manifest.icons.outline];
+
+// Create Temp Directory
+const tempName = fs.mkdtempSync("temp-");
+const tempPath = path.join(root, tempName);
+//fs.mkdirSync(tempPath);
+console.log(tempPath);
+
+// copy required files to temp directory
+const fromPath = manifestPath;
+const toPath = path.join(tempPath, "manifest.json");
+fs.copyFileSync(fromPath, toPath);
+
+icons.forEach((iconPath) => {
+    const fromPath = path.join(root, iconPath);
+    const toPath = path.join(tempPath, iconPath);
+    fs.mkdirSync(path.dirname(toPath), { recursive: true });
+    fs.copyFileSync(fromPath, toPath);
+});
+
+// Create zip folder
+const zipPathIn = tempPath;
+const zipPathOut = path.join(root, "appPackage.zip");
+if (fs.existsSync(zipPathOut)) {
+    fs.rmSync(zipPathOut);
+}
+
+const files = fs.readdirSync(zipPathIn);
+
+const command = `tar.exe -a --create -v --file ${zipPathOut} ${files.join(" ")}`;
+console.log(command);
+child_process.execSync(command, { cwd: zipPathIn });
+
+// Cleanup
+fs.rmdirSync(tempPath, { recursive: true, force: true });
