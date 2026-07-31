40a62de1c04b70c5c7bc9e4c60c9c8e409ccf66c
wandyezj <wandyezj@gmail.com>
2026-07-31 10:57:41 -0700

    Add docs to sideload xml manifest via catalogue

---
+++ docs/sideload-xml.md
@@ -0,0 +1,12 @@
+# Sideload the Build Add-In locally
+
+1. Create a Local share and add to trusted catalogue
+    - Run as admin: [create-manifest-share.cmd](../scripts//create-manifest-share.cmd)
+2. Download the Build Add-In Manifest
+    - [Word, Excel, and PowerPoint](https://store.office.com/app/download?assetid=WA200006798)
+    - [Outlook](https://store.office.com/app/download?assetid=WA200006932)
+3. Copy the downloaded manifest to the share
+
+## Reference
+
+[Use Network Share Catalogue on Windows](https://learn.microsoft.com/en-us/office/dev/add-ins/testing/create-a-network-shared-folder-catalog-for-task-pane-and-content-add-ins)
--- scripts/create-manifest-share.cmd
+++ scripts/create-manifest-share.cmd
@@ -1,6 +1,20 @@
 @echo off
 setlocal
 :: Must run as administrator
+
+:: Check for administrative privileges
+net session >nul 2>&1
+
+if %errorlevel% == 0 (
+    echo Success: Running as Administrator.
+    goto :main
+) else (
+    echo Failure: Please right-click and Run as Administrator.
+    pause
+    exit /b
+)
+
+
 :: Create a share for the manifest files
 set share=C:\manifests
 
@@ -18,6 +32,17 @@ md %share%
 
 :: share the folder
 net share manifests=%share%
+
+:: Add the Office Trusted Catalog registry keys
+set guid=50473000-0000-0000-0000-000000000000
+set share_name=%COMPUTERNAME%\manifests
+
+reg add "HKCU\Software\Microsoft\Office\16.0\WEF\TrustedCatalogs\{%guid%}" /v "Id" /t REG_SZ /d "{%guid%}" /f
+reg add "HKCU\Software\Microsoft\Office\16.0\WEF\TrustedCatalogs\{%guid%}" /v "Url" /t REG_SZ /d "\\%share_name%" /f
+reg add "HKCU\Software\Microsoft\Office\16.0\WEF\TrustedCatalogs\{%guid%}" /v "Flags" /t REG_DWORD /d 1 /f
+
+echo Trusted catalog registry keys configured successfully.
+
 
 :: open the folder
 start %share%
